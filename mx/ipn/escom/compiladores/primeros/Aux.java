d00ca66b5f92d3d293d5655496f17cc7c1bafcc2
AlanDSigala <asigalam1300@alumno.ipn.mx>
2023-05-19 00:37:49 -0600

    No se que pedo

--- mx/ipn/escom/compiladores/primeros/Aux.java
+++
@@ -1,9 +0,0 @@
-package mx.ipn.escom.compiladores.primeros;
-
-import mx.ipn.escom.compiladores.TipoToken;
-
-public class Aux extends Assigment_2{
-  protected Aux(){
-    super.primeros.add(TipoToken.IGUAL);
-  }
-}
--- mx/ipn/escom/compiladores/primeros/Declaration.java
+++
@@ -1,9 +0,0 @@
-package mx.ipn.escom.compiladores.primeros;
-
-public class Declaration extends Class_decl{
-  protected Declaration(){
-    super.primeros.addAll(Primeros.fun_decl.primeros);
-    super.primeros.addAll(Primeros.var_decl.primeros);
-    super.primeros.addAll(Primeros.statement.primeros);
-  }
-}
