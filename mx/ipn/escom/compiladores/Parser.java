316a3c1a62234d438c9f0cd00e99b22e125226e2
AlanDSigala <asigalam1300@alumno.ipn.mx>
2023-05-20 14:07:18 -0600

    Avance de la gramatica.

--- mx/ipn/escom/compiladores/Parser.java
+++ mx/ipn/escom/compiladores/Parser.java
@@ -1,6 +1,7 @@
 package mx.ipn.escom.compiladores;
 
 import mx.ipn.escom.compiladores.primeros.Class_inher;
+import mx.ipn.escom.compiladores.primeros.Parameters_2;
 import mx.ipn.escom.compiladores.primeros.Primeros;
 
 import java.util.List;
@@ -55,16 +56,19 @@ public class Parser {
             coincidir(TipoToken.IDENTIFICADOR);
             Class_inher();
             coincidir(TipoToken.LLAVE_IZQ);
+            Functions();
+            coincidir(TipoToken.LLAVE_DER);
 
         }else {
             System.out.println("Error en la linea " + preanalisis.linea + " posicion " + preanalisis.col + ". Se esperaba un  " + TipoToken.CLASE);
         }
-
-
     }
 
     public void Fun_decl(){
-
+        if (preanalisis.equals(TipoToken.FUNCION)){
+            coincidir(TipoToken.FUNCION);
+            Function();
+        }
     }
 
     public void Var_decl(){
@@ -75,19 +79,110 @@ public class Parser {
 
     }
 
-    public void Class_inher(){
-        coincidir(TipoToken.MENOR);
-    }
-
-    public void Funtions(){
-        if (Primeros.function.find(preanalisis.tipo)){
-            Funtions();
+    public void Var_init(){
+        if (preanalisis.equals(TipoToken.IGUAL)){
+            coincidir(TipoToken.IGUAL);
         }
     }
-    public void Funtion(){
+    public void Expression(){
+        if (Primeros.assigment.find(preanalisis.tipo)){
+            Assignment();
+        }
+    }
+
+    private void Assignment() {
+        if (Primeros.call_opc.find(preanalisis.tipo)){
+            Call_opc();
+        }
+    }
+
+    private void Call_opc() {
+        if (Primeros.call.find(preanalisis.tipo)){
+            Call();
+        }
+
+    }
+
+    private void Call() {
+        if (Primeros.primary.find(preanalisis.tipo)){
+            Primary();
+        }
+    }
+
+    private void Primary() {
+        if (preanalisis.equals(TipoToken.VERDADERO)){
+            coincidir(TipoToken.VERDADERO);
+            coincidir(TipoToken.FALSO);
+            coincidir(TipoToken.NULO);
+            coincidir(TipoToken.ESTE);
+            coincidir(TipoToken.NUMERO);
+            coincidir(TipoToken.CADENA);
+            coincidir(TipoToken.IDENTIFICADOR);
+            coincidir(TipoToken.PAREN_IZQ);
+            Expression();
+            coincidir(TipoToken.PAREN_DER);
+            coincidir(TipoToken.SUPER);
+            coincidir(TipoToken.PUNTO);
+            coincidir(TipoToken.IDENTIFICADOR);
+        }
+    }
+
+    public void Class_inher(){
+        if (preanalisis.equals(TipoToken.MENOR)){
+            coincidir(TipoToken.MENOR);
+            coincidir(TipoToken.IDENTIFICADOR);
+        }
+
+    }
+
+    public void Functions(){
+        if (Primeros.function.find(preanalisis.tipo)){
+            Function();
+            Functions();
+        }
+    }
+    public void Function(){
         if (preanalisis.equals(TipoToken.IDENTIFICADOR)){
             coincidir(TipoToken.IDENTIFICADOR);
             coincidir(TipoToken.PAREN_IZQ);
+            Parameters_opc();
+            coincidir(TipoToken.PAREN_DER);
+            Block();
+        }
+    }
+
+    public void Parameters_opc(){
+        if (Primeros.parameters_opc.find(preanalisis.tipo)){
+            Parameters();
+        }
+    }
+
+    public void Parameters(){
+        if (preanalisis.equals(TipoToken.IDENTIFICADOR)){
+            coincidir(TipoToken.IDENTIFICADOR);
+            Parameters_2();
+        }
+    }
+
+    public void Parameters_2(){
+        if (preanalisis.equals(TipoToken.COMA)){
+            coincidir(TipoToken.COMA);
+            coincidir(TipoToken.IDENTIFICADOR);
+            Parameters_2();
+        }
+    }
+
+    public void Block(){
+        if (preanalisis.equals(TipoToken.LLAVE_IZQ)){
+            coincidir(TipoToken.LLAVE_IZQ);
+            Block_decl();
+            coincidir(TipoToken.LLAVE_DER);
+        }
+    }
+    public void Block_decl(){
+        if (Primeros.declaration.find(preanalisis.tipo)){
+            Declaration();
+            Block_decl();
         }
     }
 
